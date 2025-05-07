ea5a85a33f68b4687acde0ea44b48ed45a142e5f
Brandon Gonzalez <bgbeaconhous@gmail.com>
2025-05-07 11:36:09 -0700

    started doing login/register routes - still needs adjustment

---
+++ Backend/prisma/seed.js
@@ -0,0 +1,12 @@
+const prisma = require("../prisma");
+const seed = async () => {
+
+    
+};
+seed()
+ .then(async () => await prisma.$disconnect())
+ .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+ });
